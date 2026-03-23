15b77a0c22726d0e0dfa252a21ff7c96d67da5f7
cra2y8eans <crazybeans@163.com>
2026-03-23 16:06:46 +0800

    调整栈水位

--- src/main.cpp
+++ src/main.cpp
@@ -24,22 +24,22 @@ void setup() {
   stepper_init();
   motorInit();
   // ina226_init();
-  NTC_Init();
-  Fan_Init();
-  fault_init();
+  // NTC_Init();
+  // Fan_Init();
+  // fault_init();
 
-  xTaskCreatePinnedToCore(ledUpdate, "ledUpdate", 1024 * 10, NULL, 1, NULL, 1);
-  xTaskCreatePinnedToCore(buzzerUpdate, "buzzerUpdate", 1024 * 10, NULL, 1, NULL, 1);
-  xTaskCreatePinnedToCore(dataSent, "dataSent", 1024 * 10, NULL, 2, NULL, 0);
-  xTaskCreatePinnedToCore(esp_now_connection_check, "esp_now_connection_check", 1024 * 10, NULL, 2, NULL, 0);
-  xTaskCreatePinnedToCore(buttonTask, "ButtonTask", 1024 * 10, NULL, 1, NULL, 1);
-  xTaskCreatePinnedToCore(modeIdentify, "modeIdentify", 1024 * 10, NULL, 1, NULL, 1);
-  xTaskCreatePinnedToCore(motorControl, "motorControl", 1024 * 16, NULL, 2, NULL, 1);
-  xTaskCreatePinnedToCore(NTC_task, "temperatureRead", 1024 * 10, NULL, 1, NULL, 1);
-  xTaskCreatePinnedToCore(stepper_control_task, "stepper_control_task", 1024 * 10, NULL, 3, NULL, 1);
-  xTaskCreatePinnedToCore(ina226_task, "ina226_task", 1024 * 10, NULL, 1, NULL, 1);
-  xTaskCreatePinnedToCore(Fan_task, "Fan_Task", 1024 * 10, NULL, 1, NULL, 1);
-  xTaskCreatePinnedToCore(fault_task, "fault_task", 1024 * 10, NULL, 3, &faultTaskHandle, 1);
+  xTaskCreatePinnedToCore(ledUpdate, "ledUpdate", 1024 * 3, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(buzzerUpdate, "buzzerUpdate", 1024 * 3, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(dataSent, "dataSent", 1024 * 4, NULL, 2, NULL, 0);
+  xTaskCreatePinnedToCore(esp_now_connection_check, "esp_now_connection_check", 1024 * 4, NULL, 2, NULL, 0);
+  xTaskCreatePinnedToCore(buttonTask, "ButtonTask", 1024 * 3, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(modeIdentify, "modeIdentify", 1024 * 3, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(motorControl, "motorControl", 1024 * 4, NULL, 2, NULL, 1);
+  // xTaskCreatePinnedToCore(NTC_task, "temperatureRead", 1024 * 2, NULL, 1, NULL, 1);
+  xTaskCreatePinnedToCore(stepper_control_task, "stepper_control_task", 1024 * 4, NULL, 3, NULL, 1);
+  // xTaskCreatePinnedToCore(ina226_task, "ina226_task", 1024 * 3, NULL, 1, NULL, 1);
+  // xTaskCreatePinnedToCore(Fan_task, "Fan_Task", 1024 * 2, NULL, 1, NULL, 1);
+  // xTaskCreatePinnedToCore(fault_task, "fault_task", 1024 * 3, NULL, 3, &faultTaskHandle, 1);
 }
 
 void loop() {
